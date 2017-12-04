df3ba92f096e60ef015b6927df4c1684a94de60f
chenkaiwang <chenkaiwang@yuewen.com>
2017-12-04 12:16:30 +0800

    add json log

--- WebLog.php
+++ WebLog.php
@@ -10,21 +10,19 @@ namespace weblib\log;
 class WebLog
 {
     // 日志存放路径
-    protected static $logpath = '/data/logs/service/';
-
-    protected static $separator = "===== Separator ===== \n";
+    protected static $logpath =TSF_TAF_LOG_PATH;
 
     /**
      * 设置日志路径
      * @param $path
      * @return bool
      */
-    public static function setLogpath($path = '') {
-        if(!empty($path) && file_exists($path)) {
+    public static function setLogpath($path = '')
+    {
+        if (!empty($path) && file_exists($path)) {
             self::$logpath = $path;
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
@@ -35,24 +33,15 @@ class WebLog
      * @param $r
      * @param $method
      */
-    public static function info($msg, $r, $method, $df = null, $df2 = null)
+    public static function info($msg, $r, $method, $df = null, $df2 = null, $otherMsgArr = [])
     {
-        $time = date('m-d H:i:s');
-        $list = explode('::', $method);
-        $class = $list[0];
-        $method = $list[1];
-        if (isset($df)) {
-            $df = "[userId={$df}]";
+        if (isset(\ENVConst::$log_path) && !empty(\ENVConst::$log_path)) {
+            self::setLogPath(\ENVConst::$log_path);
         }
-        if (isset($df2)) {
-            $df2 = "[bookId={$df2}]";
+        $logLevel = \ENVConst::LOG_LEVEL;
+        if ($logLevel >= 4){
+            self::logJson($msg, "info", $otherMsgArr, $r, $method, $df, $df2);
         }
-        $msg = "[{$time}][info][{$method}][r={$r}]{$df}{$df2}" . $msg . "\n";
-        $msg = $msg . self::$separator;
-
-        $class = str_replace('\\', '_', $class) ;
-
-        error_log($msg, 3, self::$logpath . $class . '.log');
     }
 
     /**
@@ -61,24 +50,12 @@ class WebLog
      * @param $r
      * @param $method
      */
-    public static function error($msg, $r, $method, $df = null, $df2 = null)
+    public static function error($msg, $r, $method, $df = null, $df2 = null, $otherMsgArr = [])
     {
-        $time = date('m-d H:i:s');
-        $list = explode('::', $method);
-        $class = $list[0];
-        $method = $list[1];
-        if (isset($df)) {
-            $df = "[userId={$df}]";
+        if (isset(\ENVConst::$log_path) && !empty(\ENVConst::$log_path)) {
+            self::setLogPath(\ENVConst::$log_path);
         }
-        if (isset($df2)) {
-            $df2 = "[bookId={$df2}]";
-        }
-        $msg = "[{$time}][error][{$method}][r={$r}]{$df}{$df2}" . $msg . "\n";
-        $msg = $msg . self::$separator;
-
-        $class = str_replace('\\', '_', $class) ;
-
-        error_log($msg, 3, self::$logpath . $class . '.log');
+        self::logJson($msg, "error", $otherMsgArr, $r, $method, $df, $df2);
     }
 
     /**
@@ -87,23 +64,41 @@ class WebLog
      * @param $r
      * @param $method
      */
-    public static function debug($msg, $r, $method, $df = null, $df2 = null)
+    public static function debug($msg, $r, $method, $df = null, $df2 = null, $otherMsgArr = [])
     {
-        $time = date('m-d H:i:s');
-        $list = explode('::', $method);
+        if (isset(\ENVConst::$log_path) && !empty(\ENVConst::$log_path)) {
+            self::setLogPath(\ENVConst::$log_path);
+        }
+        $logLevel = \ENVConst::LOG_LEVEL;
+        if ($logLevel >= 5)
+            self::logJson($msg, "debug", $otherMsgArr, $r, $method, $df, $df2);
+    }
+
+    public static function logJson($msg, $level = 'info', $otherMsgArr = [], $r = 0, $class = 0, $df = null, $df2 = null)
+    {
+        $list = explode('::', $class);
         $class = $list[0];
+        $class = str_replace('\\', '_', $class);
         $method = $list[1];
+
+        $logArray = $otherMsgArr;
+        $logArray['msg'] = $msg;
+        $logArray['time'] = date('m-d H:i:s');
+        $logArray['method'] = $method;
+        $logArray['class'] = $class;
         if (isset($df)) {
-            $df = "[userId={$df}]";
+            $logArray['ywGuid'] = $df;
         }
         if (isset($df2)) {
-            $df2 = "[bookId={$df2}]";
+            $logArray['bookId'] = $df2;
         }
-        $msg = "[{$time}][debug][{$method}][r={$r}]{$df}{$df2}" . $msg . "\n";
-        $msg = $msg . self::$separator;
+        $logArray['r'] = $r;
+        $logArray['level'] = $level;
 
-        $class = str_replace('\\', '_', $class) ;
+        $msg = json_encode($logArray, JSON_UNESCAPED_UNICODE) . "\n\r";
 
-        error_log($msg, 3, self::$logpath . $class . '.log');
+        error_log($msg, 3, self::$logpath . $class . '.' . date('Y-m-d') . '.log');
+
+        return $msg;
     }
 }
